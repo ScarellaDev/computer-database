8c7d856d8c03ab58898012205118e8aaa988571a
Jeremy Scarella <scarella.dev@gmail.com>
2015-01-15 10:30:12 +0100

    15-01-2015: maven migration

---
+++ src/test/java/com/excilys/computerdatabase/test/persistence/CompanyDaoTest.java
@@ -0,0 +1,22 @@
+package com.excilys.computerdatabase.test.persistence;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class CompanyDaoTest extends TestCase {
+
+  public CompanyDaoTest(String testMethodName) {
+    super(testMethodName);
+  }
+    
+  @Test
+  public void testGetById() {
+
+  }
+  
+  @Test
+  public void testGetAll() {
+
+  }
+}
